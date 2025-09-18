7ed1faad214f04826eafab821f15dd6ad5e3439c
Justice <nkopane.mj@gmail.com>
2025-09-18 19:29:01 +0200

    commit 2

--- src/main/java/com/example/SaveUserLambda.java
+++ src/main/java/com/example/SaveUserLambda.java
@@ -3,13 +3,18 @@ package com.example;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class SaveUserLambda implements RequestHandler<String, String> {
+public class SaveUserLambda implements RequestHandler<UserRequest, String> {
 
     @Override
-    public String handleRequest(String s, Context context) {
-        context.getLogger().log("input :" + s);
+    public String handleRequest(UserRequest userRequest, Context context) {
+        if(userRequest == null)
+            throw new IllegalArgumentException("Bad Request");
 
-        if(s == null || s.isEmpty()) s = "World!";
-        return "Hello " + s;
+        String firstName = userRequest.getFirstName();
+        String lastName = userRequest.getLastName();
+
+        context.getLogger().log("input :" + firstName + " " + lastName);
+
+        return "Hello " + firstName + " " + lastName;
     }
 }
